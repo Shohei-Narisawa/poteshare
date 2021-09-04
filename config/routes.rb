951164f55eef3d59488fe1eae14f2d615b47f86a
Shohei-Narisawa <j171leg@gmail.com>
2021-09-04 11:55:10 +0000

    update README

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'rooms/index' => 'rooms#index'
+  get 'rooms/new' => 'rooms#new'
   get '/' => 'home#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :rooms
 end
