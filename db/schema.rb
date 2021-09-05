0626194211e934d4edb997d9bfd30bf8ebd6e44d
Shohei-Narisawa <j171leg@gmail.com>
2021-09-05 07:17:51 +0000

    update README

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_04_091954) do
+ActiveRecord::Schema.define(version: 2021_09_05_065842) do
 
   create_table "rooms", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2021_09_04_091954) do
     t.string "address"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "img"
   end
 
 end
