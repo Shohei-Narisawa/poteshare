4c6bbb7ea1560cf5d415cf5331223cfff2529376
Shohei-Narisawa <j171leg@gmail.com>
2021-09-09 11:31:04 +0000

    update Readme

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -8,8 +8,13 @@ class RoomsController < ApplicationController
   end
   
   def create
-    @room = Room.new(name: params[:name], introduction: params[:introduction], price: params[:price], address: params[:address], img: params[:img])
-    @room.image_name = "#{@room.id}.jpg"
+    @room = Room.new(name: params[:name], introduction: params[:introduction], price: params[:price], address: params[:address])
+    if image = params[:image]
+      @room.image_name = "#{@room.id}.jpg"
+      image = params[:image]
+      File.binwrite("public/room_images/#{@room.image_name}", image.read )
+    end
+    
     if @room.save
       redirect_to :rooms
     else
@@ -18,6 +23,7 @@ class RoomsController < ApplicationController
   end
 
   def show
+    @room = Room.find(params[:id])
   end
 
   def edit
@@ -28,5 +34,8 @@ class RoomsController < ApplicationController
   end
 
   def destroy
+    @room = Room.find_by(id: params[:id])
+    @room.destroy
+    redirect_to :rooms
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   get 'rooms/index' => 'rooms#index'
   get 'rooms/new' => 'rooms#new'
   post 'rooms/create' => 'rooms#create'
+  post 'rooms/:id' => 'rooms#show'
+  post 'rooms/:id/destroy' => 'rooms#destroy'
   get '/' => 'home#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :rooms
